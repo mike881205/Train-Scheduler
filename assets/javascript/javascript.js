ed8e30549f863f944040950a51949202b0c7dce7
mike881205 <mike881205@gmail.com>
2019-10-29 13:55:29 -0400

    added moment.js script, and time conversion formulas to calculate the next arrival time and minutes away time

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -16,12 +16,19 @@ $("#submit-info").on("click", function () {
     let firstTrain = $("#first-input").val().trim();
     let trainFrequency = $("#frequency-input").val().trim();
 
+    let firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+    let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    let tRemainder = diffTime % trainFrequency;
+    let tMinutesTillTrain = trainFrequency - tRemainder;
+    // let nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
+
+
     database.ref().push({
         name: trainName,
         destination: trainDestination,
-        firstTrain: firstTrain,
         frequency: trainFrequency,
-
+        // next: nextTrain,
+        minAway: tMinutesTillTrain
     });
 
     $("#name-input").val("");
@@ -35,6 +42,8 @@ database.ref().on("child_added", function (snapshot) {
     let name = snapshot.val().name
     let destination = snapshot.val().destination
     let frequency = snapshot.val().frequency
+    let next = snapshot.val().next
+    let minAway = snapshot.val().minAway
 
     // for (let i = 0; i < database.length; i++) {
 
@@ -49,6 +58,12 @@ database.ref().on("child_added", function (snapshot) {
 
         let tdFrequency = $("<td>").text(frequency);
         newRow.append(tdFrequency);
+
+        let tdNext = $("<td>").text(next);
+        newRow.append(tdNext);
+
+        let tdMinAway = $("<td>").text(minAway);
+        newRow.append(tdMinAway);
 
         $("#trainInfo").append(newRow);
 
